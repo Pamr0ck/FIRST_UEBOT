8df1f7728165e3dc05eeb2e6baf18412960dee7f
u-tain <43996253+u-tain@users.noreply.github.com>
2018-12-01 16:15:26 +0300

    Update BOOOOOOT.py

--- BOOOOOOT.py
+++ BOOOOOOT.py
@@ -6,6 +6,13 @@ queue = deque()  # массив с возможностью расширения
 # q.popleft()-удаляет нулевой элемент из массива и сдвигает все элементы на 1 влево
 bot = telebot.TeleBot("782381386AAFLzg8wce1km24O2sspt_ObKHUwMeA_5yc")
 
+@bot.message_handler(commands=['start', 'go'])
+def handle_start(message):
+        user_markup = telebot.types.ReplyKeyboardMarkup(true, false)
+        user_markup.row("Хочу кофе", "Длина очереди?")
+        user_markup.row("какой я по счету?", "Пропустить одного человека вперед", "Выйти из очереди")
+        bot.send_message(message.chat.id, "Добро пожаловать:", reply_markup=user_markup)
+        
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
     if message.text == "Hi":
