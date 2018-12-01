a2c6c90ea489c4824a0ef9a7370c94bd36c61b16
kiril431 <45508584+kiril431@users.noreply.github.com>
2018-12-01 16:14:34 +0300

    Add files via upload

---
+++ BOOOOOOT.py
@@ -0,0 +1,58 @@
+import telebot
+from collections import deque
+
+queue = deque()  # массив с возможностью расширения и сжатия
+# queue.append('name')-добавляет ячейку с информацией (name) в массив queue
+# q.popleft()-удаляет нулевой элемент из массива и сдвигает все элементы на 1 влево
+bot = telebot.TeleBot("782381386AAFLzg8wce1km24O2sspt_ObKHUwMeA_5yc")
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    if message.text == "Hi":
+        bot.send_message(message.from_user.id, "Hello! Want some..?")
+
+    elif message.text == "How are you?" or message.text == "How are u?":
+        bot.send_message(message.from_user.id, "Better than you while you haven't any coffee")
+
+    else:  bot.send_message(message.from_user.id, "I can't understand you :c")
+
+@bot.message_handler(command=["How_are_you"])
+def handle_text(message):
+    bot.send_message(message.chat.id, "I'm fine, thanks. And you?")
+
+@bot.message_handler(command=["Coffee"])
+def handle_text(message):
+    bot.send_message(message.chat.id, "You are added to the stack, well done! :)")
+    queue.append(chat.id)
+
+@bot.message_handler(command=["Step_ahead"])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Ok.....")
+    N=queue.index(chat.id)
+    queue[N+1],queue[N]=queue[N],queue[N+1]
+
+@bot.message_handler(command=["My_number"])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Ok.....Your number is ", queue.index(chat.id))
+
+
+@bot.message_handler(command=["Show_queue"])
+def handle_text(message):
+    for i in queue:
+        bot.send_message(message.chat.id, i)
+
+@bot.message_handler(command=["Finish"])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Oh, I didn't expect you to be such a one-minute man!")
+    bot.send.message(message.queue[1],"You are next!!!!!!!!!")
+    queue.popleft()
+
+
+
+    elif message.text == 'Finish':  # выйти из очереди
+        bot.send_message(message.from_user.id, "Oh, I didn't expect you to be such a one-minute man!")
+        bot.send_message(message.from_user.id, 'you next')
+
+
+    else:
+        bot.send_message(message.from_user.id, "Sorry, i dont understand you.")
